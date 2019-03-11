995d2c2f321a1fd7abca4a1c008232ad488d99f3
thierry DE AZEVEDO <tshlda07911@gmail.com>
2019-03-11 14:09:12 +0100

    SC maj part 1 + part 2 and 3 done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,18 @@
 class QuestionsController < ApplicationController
+
   def ask
+
   end
 
   def answer
+    @ask = params[:question]
+    if @ask == 'Hello'
+      @answer = 'Great!'
+    elsif @ask.include? '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+
+    end
   end
 end
